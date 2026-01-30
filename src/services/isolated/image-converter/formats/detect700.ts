c5070f0411c3174600462d3aa9dbaa7053ef98c3
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-01-30 14:59:14 +0530

    converted Box to type

--- src/services/isolated/image-converter/formats/detect700.ts
+++ src/services/isolated/image-converter/formats/detect700.ts
@@ -1,5 +1,5 @@
 import sharp from "sharp";
-import { Box, FaceDetection } from "@vladmandic/face-api";
+import type { Box, FaceDetection } from "@vladmandic/face-api";
 import { loadImage } from "../../../../common/env.js";
 import { faceDetectionNet, faceDetectionOptions } from "../../../../common/faceDetection.js";
 import faceApi from "../../../../common/faceapi.js";
