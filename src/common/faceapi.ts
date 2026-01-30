23b4db14b0ffde08b6d65eae28440a4ff0be7958
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-01-30 16:25:57 +0530

    vb

--- src/common/faceapi.ts
+++ src/common/faceapi.ts
@@ -1,3 +1,3 @@
-import * as faceApi from "face-api.js";
+import * as faceApi from "@vladmandic/face-api/dist/face-api.node-cpu.js";
 export default faceApi;
 
--- src/services/isolated/image-converter/formats/detect700.ts
+++ src/services/isolated/image-converter/formats/detect700.ts
@@ -1,5 +1,5 @@
 import sharp from "sharp";
-import type { Box, FaceDetection } from "face-api.js";
+import type { Box, FaceDetection } from "@vladmandic/face-api";
 import { Canvas, loadImageData } from "../../../../common/env.js";
 import { faceDetectionNet, faceDetectionOptions } from "../../../../common/faceDetection.js";
 import faceApi from "../../../../common/faceapi.js";
