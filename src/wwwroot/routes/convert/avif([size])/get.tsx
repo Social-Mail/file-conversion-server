8c33d9185c953e44201b4b3bb8cb7637bdfc2beb
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-09-15 22:09:09 +0530

    scale fixed

--- src/wwwroot/routes/convert/avif([size])/get.tsx
+++ src/wwwroot/routes/convert/avif([size])/get.tsx
@@ -40,7 +40,6 @@ export default class extends BaseConverterPage {
         await spawnPromise("/ffmpeg/ffmpeg", [
             "-i",
             inputFile,
-            `-map`,`0:v:1?`,`-map`,`0:v:0`,`-disposition:v:0 none`,
             "-vf", `scale='if(gt(ih,${size}),-2,iw)':'if(gt(ih,${size}),${size},ih)'`,
             "-c:v", "libsvtav1",
             "-crf", "12",
