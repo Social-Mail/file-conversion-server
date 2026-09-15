44684d94f2aaf8be5baefc4abe090c9525ddb2c4
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-09-15 12:59:57 +0530

    added avif resize

---
+++ src/wwwroot/routes/convert/avif([size])/get.tsx
@@ -0,0 +1,37 @@
+import { Route } from "@entity-access/server-pages/dist/core/Route.js";
+import LockFile from "../../../../core/LockFile.js";
+import { spawnPromise } from "../../../../core/spawnPromise.js";
+import BaseConverterPage from "../BaseConverterPage.js";
+
+export default class extends BaseConverterPage {
+
+    @Route
+    size: any;
+
+    async convert({
+        input,
+        output,
+        args
+    }) {
+
+        using _lock = await LockFile.lock("video-conversion");
+
+        const { size } = this;
+
+        await spawnPromise("/ffmpeg/ffmpeg", [
+            "-i",
+            input.path,
+            "-vf", `scale='if(gt(ih,${size}),-2,iw)':'if(gt(ih,${size}),${size},ih)'`,
+            "-c:v", "libsvtav1",
+            "-crf", "12",
+            "-an",
+            "-loop", "0",
+            ... args,
+            "-y",
+            output.path
+        ]); 
+
+        return output;
+    }
+
+}
